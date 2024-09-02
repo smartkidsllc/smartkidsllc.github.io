60f60eb8a37fb07ef81a981b8c99d7651adb91fc
Jason Yundt <jason@jasonyundt.email>
2024-09-02 10:50:30 -0400

    Improve home page’s title
    Before this change, the home page’s title was
    “Hello from SmartKids Docs | SmartKids Docs”. This commit improves the
    home page’s title by making it less redundant.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home(): JSX.Element {
     const { siteConfig } = useDocusaurusContext();
     return (
         <Layout
-            title={`Hello from ${siteConfig.title}`}
+            title="Home"
             description={siteConfig.tagline}
         >
             <HomepageHeader />
