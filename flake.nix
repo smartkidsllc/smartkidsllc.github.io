957d9ab547d470fc9f710f57669a2410ab5abec4
Jason Yundt <jason@jasonyundt.email>
2024-11-07 11:48:19 -0500

    Use better workaround for flake limitation
    Before this change, I had manually wrote a function that would create
    multiple flake outputs for different architectures. This commit uses a
    function written by someone else. That function simplifies our code.
    
    The main motivation behind this change is to make it easier to create a
    future commit. That future commit will add flake outputs that aren’t
    devShells. By making this change now, I won’t have to update the
    function that I wrote manually in the future commit to support things
    other than devShells.

--- flake.nix
+++ flake.nix
@@ -1,27 +1,27 @@
 {
   description = "A flake for working on smartkidsllc.github.io";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+  };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      devShellsFor =
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          "${system}".default = pkgs.mkShellNoCC {
-            name = "shell-for-working-on-smartkidsllc.github.io";
-            packages = [ pkgs.corepack ];
-          };
-        };
-    in
     {
-      # This is a workaround for this issue:
-      # <https://github.com/NixOS/nix/issues/3843>
-      devShells =
-        (devShellsFor "aarch64-darwin") // (devShellsFor "x86_64-darwin") // (devShellsFor "x86_64-linux");
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          name = "shell-for-working-on-smartkidsllc.github.io";
+          packages = [ pkgs.corepack ];
+        };
+      }
+    );
 }
