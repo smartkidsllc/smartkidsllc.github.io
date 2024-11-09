c3bf5d4d06313d4f741bfaab9f36f266bc0672e0
Jason Yundt <jason@jasonyundt.email>
2024-11-09 08:24:54 -0500

    Rename update-script to update-script-part-1
    The idea here is to eventually have two update scripts.
    update-script-part-1 will do all of the updates that are related to Nix,
    and update-script-part-2 will do all of the updates that aren’t related
    to Nix. There’s two reasons why I want to do this:
    
    1. After flake.nix and flake.lock are updated, we should really invoke
    nix a second time in order to do the rest of the updates. That way the
    rest of the updates are done using the new version of the flake rather
    than the old version of the flake.
    
    2. I want to make it so that update-script-part-2 can be run even if you
    don’t have the Nix package manager. You don’t need to have the Nix
    package manager in order to use this repo, so you shouldn’t need to have
    the Nix package manager in order to update this repo. At least, you
    shouldn’t need to have the Nix package manager in order to update the
    parts of this repo that have nothing to do with Nix.

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "A flake for working on smartkidsllc.github.io";
 
   inputs = {
-    # The update script will automatically keep this input up to date:
+    # update-script-part-1 will automatically keep this input up to date:
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    # The update script will automatically keep this input up to date:
+    # update-script-part-1 will automatically keep this input up to date:
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
   };
 
@@ -20,71 +20,73 @@
         pkgs = nixpkgs.legacyPackages."${system}";
       in
       {
-        packages.update-script =
-          pkgs.resholve.writeScriptBin "update-script"
-            {
-              inputs = [
-                pkgs.coreutils
-                pkgs.gh
-                pkgs.gitMinimal
-                pkgs.gnused
-                pkgs.nix
-              ];
-              execer = [
-                "cannot:${pkgs.lib.meta.getExe pkgs.nix}"
-                # TODO: These next ones can be removed when they’re fixed
-                # upstream:
-                "cannot:${pkgs.lib.meta.getExe pkgs.gh}"
-                "cannot:${pkgs.lib.meta.getExe pkgs.gitMinimal}"
-              ];
-              interpreter = pkgs.lib.meta.getExe pkgs.bash;
-            }
-            ''
-              set -o errexit -o nounset -o pipefail
-
-              function run_nix {
-                nix \
-                  --extra-experimental-features 'nix-command flakes' \
-                  "$@"
+        packages = {
+          update-script-part-1 =
+            pkgs.resholve.writeScriptBin "update-script-part-1"
+              {
+                inputs = [
+                  pkgs.coreutils
+                  pkgs.gh
+                  pkgs.gitMinimal
+                  pkgs.gnused
+                  pkgs.nix
+                ];
+                execer = [
+                  "cannot:${pkgs.lib.meta.getExe pkgs.nix}"
+                  # TODO: These next ones can be removed when they’re fixed
+                  # upstream:
+                  "cannot:${pkgs.lib.meta.getExe pkgs.gh}"
+                  "cannot:${pkgs.lib.meta.getExe pkgs.gitMinimal}"
+                ];
+                interpreter = pkgs.lib.meta.getExe pkgs.bash;
               }
+              ''
+                set -o errexit -o nounset -o pipefail
 
-              function get_flake_input_branch_or_tag {
-                local -r input_name="$1"
-                local flake_url
-                flake_url="$(run_nix eval \
-                  --impure \
-                  --raw \
-                  --expr "(import ./flake.nix).inputs.$input_name.url"
+                function run_nix {
+                  nix \
+                    --extra-experimental-features 'nix-command flakes' \
+                    "$@"
+                }
+
+                function get_flake_input_branch_or_tag {
+                  local -r input_name="$1"
+                  local flake_url
+                  flake_url="$(run_nix eval \
+                    --impure \
+                    --raw \
+                    --expr "(import ./flake.nix).inputs.$input_name.url"
+                  )"
+                  readonly flake_url
+                  printf '%s' "$flake_url" | cut --delimiter=/ --fields=3
+                }
+
+                # Flake inputs
+                ## nixpkgs
+                current_nixpkgs_branch="$(get_flake_input_branch_or_tag nixpkgs)"
+                readonly current_nixpkgs_branch
+                latest_nixpkgs_branch="$(git ls-remote --heads https://github.com/NixOS/nixpkgs 'nixos-??.??' \
+                  | tail --lines=1 \
+                  | cut --fields=2 \
+                  | cut --delimiter=/ --fields=3
                 )"
-                readonly flake_url
-                printf '%s' "$flake_url" | cut --delimiter=/ --fields=3
-              }
+                if [ "$current_nixpkgs_branch" != "$latest_nixpkgs_branch" ]
+                then
+                  sed --in-place "s/$current_nixpkgs_branch/$latest_nixpkgs_branch/g" flake.nix
+                fi
 
-              # Flake inputs
-              ## nixpkgs
-              current_nixpkgs_branch="$(get_flake_input_branch_or_tag nixpkgs)"
-              readonly current_nixpkgs_branch
-              latest_nixpkgs_branch="$(git ls-remote --heads https://github.com/NixOS/nixpkgs 'nixos-??.??' \
-                | tail --lines=1 \
-                | cut --fields=2 \
-                | cut --delimiter=/ --fields=3
-              )"
-              if [ "$current_nixpkgs_branch" != "$latest_nixpkgs_branch" ]
-              then
-                sed --in-place "s/$current_nixpkgs_branch/$latest_nixpkgs_branch/g" flake.nix
-              fi
+                ## flake-utils
+                current_flake_utils_tag="$(get_flake_input_branch_or_tag flake-utils)"
+                latest_flake_utils_tag="$(gh release view --repo numtide/flake-utils --json tagName --jq '.tagName')"
+                if [ "$current_flake_utils_tag" != "$latest_flake_utils_tag" ]
+                then
+                  sed --in-place "s/$current_flake_utils_tag/$latest_flake_utils_tag/g" flake.nix
+                fi
 
-              ## flake-utils
-              current_flake_utils_tag="$(get_flake_input_branch_or_tag flake-utils)"
-              latest_flake_utils_tag="$(gh release view --repo numtide/flake-utils --json tagName --jq '.tagName')"
-              if [ "$current_flake_utils_tag" != "$latest_flake_utils_tag" ]
-              then
-                sed --in-place "s/$current_flake_utils_tag/$latest_flake_utils_tag/g" flake.nix
-              fi
-
-              # Flake lock file
-              run_nix flake update
-            '';
+                # Flake lock file
+                run_nix flake update
+              '';
+        };
         devShells.default = pkgs.mkShellNoCC {
           name = "shell-for-working-on-smartkidsllc.github.io";
           packages = [ pkgs.corepack ];
